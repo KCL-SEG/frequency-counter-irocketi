bc148a86c3a53b62a065160329b5c1b4fa3c4c8d
Sabeeka Ahmad (k20012890) <sabeeka.1.ahmad@kcl.ac.uk>
2022-10-10 21:20:07 +0100

    Attempt initial solution

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,17 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    # converting all list elements to string
+    for i in range(0, len(items)):
+        items[i] = str(items[i])
+
+    # extracting unique values
+    uniqueItems = set(items)
+
+    print("unique list: " + str(uniqueItems))
+    for x in uniqueItems:
+        count = items.count(x)
+        frequencies.update({x:count})
+
     return frequencies
